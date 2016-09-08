f429e913ac0b0b209f40abcc2a8cfd169ccc3004
Brent Sullivan <brntsllvn@gmail.com>
2016-09-07 19:30:27 -0700

    init

---
+++ KeywordDynamic/DynamicSandbox.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+
+namespace KeywordDynamic
+{
+    public class DynamicSandbox
+    {
+    }
+
+    [TestFixture]
+    public class KeywordDynamicTests
+    {
+        [Test]
+        public void Dynamic_With_String()
+        {
+            dynamic item = "First";
+            dynamic value = "!";
+
+            string result = item + value;
+
+            result.ShouldBe("First!");
+        }
+
+        [Test]
+        public void Dynamic_BinderException()
+        {
+            dynamic item = 1;
+            dynamic value = 2;
+
+            Should.Throw<RuntimeBinderException>(() => { string result = item + value; });
+        }
+
+        [Test]
+        public void Dynamic_NoException()
+        {
+            dynamic item = 1;
+            dynamic value = 2;
+
+            dynamic result = item + value;
+
+            // note: extension methods not available on dynamic types
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+
+    }
+}
